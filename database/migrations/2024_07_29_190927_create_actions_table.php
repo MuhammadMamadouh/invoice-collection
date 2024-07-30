d07a3b58b1ec2fbc0b6db73fdf29cf52490ba557
MuhammadMamadouh <muhammadmamdouh93@gmail.com>
2024-07-30 15:39:51 +0300

    update action types table name

--- database/migrations/2024_07_29_190927_create_actions_table.php
+++ database/migrations/2024_07_29_190927_create_actions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('actions', function (Blueprint $table) {
+        Schema::create('action_types', function (Blueprint $table) {
             $table->id();
             $table->string('en_name');
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('actions');
+        Schema::dropIfExists('action_types');
     }
 };
