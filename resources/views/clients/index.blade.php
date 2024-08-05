07f235e42539ab5d2a814441f8c60d8f74344020
AT0047 <aboddy.net@gmail.com>
2024-08-05 23:11:33 +0300

    "show clients data "

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-lg-3 col-md-4 col-sm-6">
                     <a href="{{ route('clients.index') }}" class="m-1 w-100 btn btn-primary p-3">
                         <i class="fa-solid fa-earth-asia"></i> &ensp; {{ __('All clients') }} &ensp;
-                        <span class="badge bg-white text-primary">{{\App\Models\Client::count()}}</span>
+                        <span class="badge bg-white text-primary">{{ \App\Models\Client::count() }}</span>
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-4 col-sm-6">
@@ -313,7 +313,10 @@
                                     </div>
                                 </div>
                                 <div class="col-2 hide">
-                                    <p class="fw-bold">Group Industrie</p>
+                                    <p class="fw-bold">{{ __('Group Industrie') }}</p>
+                                    @foreach ($client->clientsGroups as $group)
+                                        <p class=""><a href="#">{{ $group->en_name }}</a></p>
+                                    @endforeach
                                 </div>
                                 <div class="col-1 hide">
                                     <p class="fw-bold">4Front</p>
@@ -333,7 +336,7 @@
                                         <p class="fw-bold">4Front</p>
                                     </div>
                                     <div class="d-flex bg-light">
-                                        <p class="fw-bold">Collection scenario:</p>
+                                        <p class="fw-bold">{{__("Collection scenario")}}:</p>
                                         <div class="">
                                             <select name="collection_scenario_id" class="form-select" id="inputname19">
                                                 <option value="" selected disabled>{{ __('Collection Scenarios') }}
@@ -462,39 +465,46 @@
                                             </div>
                                             <div class="col-lg-4 col-sm-6 text-start">
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold">{{__("Payment term")}}:</p>
-                                                    <p class="">{{$client->payment_term}}</p>
+                                                    <p class="fw-bold">{{ __('Payment term') }}:</p>
+                                                    <p class="">{{ $client->payment_term }}</p>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold">{{__("Payment mean")}}:</p>
-                                                    <p class="">{{$client->payment_mean}}</p>
+                                                    <p class="fw-bold">{{ __('Payment mean') }}:</p>
+                                                    <p class="">{{ $client->payment_mean }}</p>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold">{{__("Guarantee World Insurer")}}:</p>
-                                                    <p class="">{{$client->insurer_guarantee}}</p>
+                                                    <p class="fw-bold">{{ __('Guarantee World Insurer') }}:</p>
+                                                    <p class="">{{ $client->insurer_guarantee }}</p>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold">{{__("Other guarantees")}}:</p>
-                                                    <p class="">{{$client->other_guarantees}}</p>
+                                                    <p class="fw-bold">{{ __('Other guarantees') }}:</p>
+                                                    <p class="">{{ $client->other_guarantees }}</p>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold">{{__("Credit Limit")}}:</p>
+                                                    <p class="fw-bold">{{ __('Credit Limit') }}:</p>
                                                     <div class="form-group">
-                                                        <input type="text" value="{{$client->credit_limit}}" class="form-control" />
+                                                        <input type="text" value="{{ $client->credit_limit }}"
+                                                            class="form-control" />
                                                     </div>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold"><a href="">{{__("Late payment penalties")}}</a>:
+                                                    <p class="fw-bold"><a
+                                                            href="">{{ __('Late payment penalties') }}</a>:
                                                     </p>
-                                                    <p class="">{{$client->late_payment_penalties == 1 ? 'Yes' : 'No'}}</p>
+                                                    <p class="">
+                                                        {{ $client->late_payment_penalties == 1 ? 'Yes' : 'No' }}</p>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold"><a href="">{{__("Recovery cost")}}</a>:</p>
-                                                    <p class="">{{$client->recovery_cost == 1 ? 'Yes' : 'No'}}</p>
+                                                    <p class="fw-bold"><a href="">{{ __('Recovery cost') }}</a>:
+                                                    </p>
+                                                    <p class="">
+                                                        {{ $client->recovery_cost == 1 ? 'Yes' : 'No' }}</p>
                                                 </div>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <p class="fw-bold"><a href="">{{__("Interactive emails")}}</a>:</p>
-                                                    <p class="">{{$client->interactive_emails == 1 ? 'Yes' : 'No'}}</p>
+                                                    <p class="fw-bold"><a
+                                                            href="">{{ __('Interactive emails') }}</a>:</p>
+                                                    <p class="">
+                                                        {{ $client->interactive_emails == 1 ? 'Yes' : 'No' }}</p>
                                                 </div>
                                             </div>
                                             <div class="col-lg-4 text-start">
@@ -506,7 +516,8 @@
                                                         <option value="1">Key accounts scenario (EN)</option>
                                                         <option value="1">Modeles actions specifiques</option>
                                                         <option value="1">Risky clients (En)</option>
-                                                        <option value="1">Scenario de relance standard (by default)</option>
+                                                        <option value="1">Scenario de relance standard (by default)
+                                                        </option>
                                                         <option value="1">Standard collection scenario</option>
                                                         <option value="1">Templates specific actions</option>
                                                     </select>
