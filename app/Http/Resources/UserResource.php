346c3d5dea1d587e539c8884f932b2a7bb15bf58
AT0047 <aboddy.net@gmail.com>
2024-08-06 17:50:22 +0300

    "UserResource"

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,24 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'active' => $this->active,
+            'email' => $this->email,
+            'password' => $this->password,
+            'phone' => $this->phone,
+            'mobile_phone' => $this->mobile_phone,
+            'function' => $this->function,
+            'role_id' => $this->role_id,
+            'picture' => $this->picture,
+            'fax' => $this->fax,
+            'id_number' => $this->id_number,
+            'email_verified_at' => $this->email_verified_at,
+            'is_super_admin' => $this->is_super_admin,
+            'rememberToken' => $this->rememberToken,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
